34ca14db6b5e5d8dc2edb8423101da41c5e874f7
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-14 19:11:33 +0530

    initializing memory

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ let canvas = document.getElementById('screen');
 let ctx = canvas.getContext('2d');
 let imageData = ctx.createImageData(160, 144);
 
+let memory = new Uint8Array(0x10000);
+
+document.getElementById('romInput').addEventListener('change', handleROMLoad);
+
