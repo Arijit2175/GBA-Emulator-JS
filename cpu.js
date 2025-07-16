26607a59c19a70940117e69765bf0d52131f2ea0
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-16 13:51:19 +0530

    reset sp and pc to valid addresses

--- cpu.js
+++ cpu.js
@@ -3,3 +3,10 @@ const cpu = {
     cpsr: 0
 }
 
+function cpuReset() {
+  cpu.registers.fill(0);
+  cpu.registers[13] = 0x03007F00; 
+  cpu.registers[15] = 0x08000000; 
+  cpu.cpsr = 0x00000030;          
+}
+
