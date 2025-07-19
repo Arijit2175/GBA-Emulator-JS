255536c700cec6cc52e84547c3e756e2806246a6
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-19 10:00:43 +0530

    modified to catch reference

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+import { CPU } from './cpu.js';
+import { memory } from './memory.js';
+
 let canvas = document.getElementById('screen');
 let ctx = canvas.getContext('2d');
 let imageData = ctx.createImageData(240, 160);
 
-window.rom = new Uint8Array(32 * 1024 * 1024);
+window.rom = new Uint8Array(32 * 1024 * 1024); 
 let romSize = 0;
+
+let cpu = new CPU();
 
 document.getElementById('romInput').addEventListener('change', handleROMLoad);
 
@@ -22,21 +27,22 @@ function handleROMLoad(event) {
 
     console.log("ROM start byte:", memory.read8(0x08000000).toString(16));
 
-    cpu.reset();      
-    cpu.run(20);       
+    cpu.reset();
+    cpu.run(20); 
+
     console.log("r0:", cpu.registers[0]);
     console.log("PC:", cpu.registers[15].toString(16));
-
   };
+
   reader.readAsArrayBuffer(file);
 }
 
 function drawPlaceholderScreen() {
   for (let i = 0; i < imageData.data.length; i += 4) {
-    imageData.data[i + 0] = 173;
-    imageData.data[i + 1] = 216;
-    imageData.data[i + 2] = 230;
-    imageData.data[i + 3] = 255;
+    imageData.data[i + 0] = 173;  
+    imageData.data[i + 1] = 216;  
+    imageData.data[i + 2] = 230;  
+    imageData.data[i + 3] = 255;  
   }
   ctx.putImageData(imageData, 0, 0);
 }
